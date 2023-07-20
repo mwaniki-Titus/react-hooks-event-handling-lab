2a4be00feea6f5612078591e64575225926cf495
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-07-20 13:38:30 +0300

    complete assingment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const handleChange = (event)=> {
+        console.log("Entering password...");
+    }
     return (
-        <div></div>
+        <div>
+            <input type ="password" onChange={handleChange}/>
+        </div>
     )
 }
 
